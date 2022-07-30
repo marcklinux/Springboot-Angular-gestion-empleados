b88bf2ea08393ded12f779651fb8385c1f5f98fb
Marco Antonio Dominguez <34685744+marcklinux@users.noreply.github.com>
2022-07-30 17:02:46 -0500

    springBoot-Angular-backend

--- gestion-empleados-backend/src/main/java/com/gestion/empleados/controlador/EmpleadoControlador.java
+++ gestion-empleados-backend/src/main/java/com/gestion/empleados/controlador/EmpleadoControlador.java
@@ -1,25 +1,80 @@
 package com.gestion.empleados.controlador;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gestion.empleados.excepciones.ResourceNotFoundException;
 import com.gestion.empleados.modelo.Empleado;
 import com.gestion.empleados.repositorios.EmpleadoRepositorio;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmpleadoControlador {
 
 	@Autowired
 	private EmpleadoRepositorio empleadoRepositorio;
 
+	//este metodo sirve para listar todos los empleados
 	@GetMapping("/empleados")
 	public List<Empleado> listarTodosLosEmpleados() {
 		return empleadoRepositorio.findAll();
+	}
+	
+	//este metodo sirve para guardar el empleado
+	@PostMapping("/empleados")
+	public Empleado guardarEmpleado(@RequestBody Empleado empleado) {
+		return empleadoRepositorio.save(empleado);
+	}
+	
+	
+	//este metodo sirve para buscar un empleado por id
+	@GetMapping("/empleados/{id}")
+	public ResponseEntity<Empleado> obtenerEmpleadoPorId(@PathVariable Long id){
+		Empleado empleado = empleadoRepositorio.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No existe el empleado con el Id :" + id));
+				
+				return ResponseEntity.ok(empleado);
+	}
+	
+	//este metodo sirve para actualizar un empleado
+	@PutMapping("/empleados/{id}")
+	public ResponseEntity<Empleado> actualizarEmpleado(@PathVariable Long id,@RequestBody Empleado detallesEmpleado){
+		Empleado empleado = empleadoRepositorio.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No existe el empleado con el Id :" + id));
+		
+		empleado.setNombre(detallesEmpleado.getNombre());
+		empleado.setApellido(detallesEmpleado.getApellido());
+		empleado.setEmail(detallesEmpleado.getEmail());
+		
+		Empleado empleadoActualizado = empleadoRepositorio.save(empleado);
+				
+				return ResponseEntity.ok(empleadoActualizado);
+	}
+	
+	//este metodo sirve para eliminar un empleado
+	@DeleteMapping("/empleados/{id}")
+	public ResponseEntity<Map<String,Boolean>> eliminarEmpleado(@PathVariable Long id){
+		Empleado empleado = empleadoRepositorio.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("No existe el empleado con el ID :" + id));
+		
+		empleadoRepositorio.delete(empleado);
+		Map<String,Boolean> respuesta = new HashMap<>();
+		respuesta.put("eliminar", Boolean.TRUE);
+		return ResponseEntity.ok(respuesta);
 	}
 
 }
